c0b58e82e20ef19096cf353b7063ea2f5e681313
Ralph-Wang <ralph_ld@aliyun.com>
2015-01-27 23:07:56 +0800

    格式化输出

--- shell.py
+++ shell.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+shell.py
+
+sqlite3 的 shell 工具
+"""
+
 import sqlite3
 import sys
 
-
 HELP_FORMAT = "{0:<10}\t{1}".format
-def help(conn=None, arg=None):
+
+
+def display_help(conn=None, arg=None):
+    """
+    输出帮助信息
+    """
     print
     print HELP_FORMAT("?", "print this help menu")
     print HELP_FORMAT("connect", "connect to other database")
     print HELP_FORMAT("quit", "quit sqlite3 shell")
     print
     return conn
+
 
 def connect(conn, file_name):
     """
@@ -21,11 +32,46 @@ def connect(conn, file_name):
     @return {sqlite3.Connection} conn 返回新的数据库连接
     """
     conn = sqlite3.connect(file_name)
+    conn.row_factory = sqlite3.Row
     conn.isolation_level = None
     return conn
 
-def quit(conn=None, arg=None):
+
+def quit_shell(conn=None, arg=None):
+    """
+    退出 shell
+    """
     exit(0)
+
+
+def display_row(row):
+    """
+    显示一行
+    """
+    result = ''
+    cell = '|{0:>10}'.format
+    for value in row:
+        result += cell(value)
+    result += '|'
+    print result
+
+
+def display(cursor):
+    """
+    格式化显示游标结果
+    """
+    for idx, row in enumerate(cursor):
+        n = len(row)
+        print '-' * (11 * n + 1)
+        if idx == 0:
+            display_row(row.keys())
+            print '-' * (11 * n + 1)
+        display_row(row)
+    try:
+        print '-' * (11 * n + 1)
+    except UnboundLocalError:
+        pass
+
 
 def execute_sql(conn, sql):
     """
@@ -38,24 +84,24 @@ def execute_sql(conn, sql):
         sql = sql.strip()
         cur = conn.execute(sql)
         if sql.lstrip().upper().startswith("SELECT"):
-            print cur.fetchall()
-    except sqlite3.Error as e:
-        print "An error occurred:", e.args[0]
+            display(cur)
+    except sqlite3.Error as err:
+        print "An error occurred:", err.args[0]
     return conn
 
 
 COMMANDS = {
-        "?": help,
-        "connect": connect,
-        "quit": quit,
-        "default": execute_sql,
-        }
+    "?": display_help,
+    "connect": connect,
+    "quit": quit_shell,
+    "default": execute_sql,
+}
 
 
-def main(db=":memory:"):
-    conn = connect(None, db)
+def main(database=":memory:"):
+    conn = connect(None, database)
 
-    buffer = ""
+    buf = ""
     print "Enter your SQL commands to execute in sqlite3."
 
     try:
@@ -65,15 +111,15 @@ def main(db=":memory:"):
             except (KeyboardInterrupt, EOFError):
                 print
                 quit()
-            buffer += line + " "
-            if sqlite3.complete_statement(buffer):
-                execute_sql(conn, buffer)
-                buffer = ""
+            buf += line + " "
+            if sqlite3.complete_statement(buf):
+                execute_sql(conn, buf)
+                buf = ""
             else:
-                cmd, args = buffer.split(" ", 1)
+                cmd, args = buf.split(" ", 1)
                 if cmd in COMMANDS:
                     conn = COMMANDS[cmd](conn, args.strip())
-                    buffer = ""
+                    buf = ""
 
     finally:
         conn.close()
